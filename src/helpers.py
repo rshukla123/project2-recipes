28888bdf8a7c4b22cf59ce6fdc9b551eb82798af
avamlie <50114050+avamlie@users.noreply.github.com>
2020-03-02 11:43:13 -0600

    added unibigrams
    gets unigrams, bigrams, and trigrams from any list

--- src/helpers.py
+++ src/helpers.py
@@ -8,3 +8,26 @@ def clean_string(s, sw):
 
 def tokenize_string(s):
 	return tokenizer().tokenize(s)
+
+
+def unibigrams(input_list):
+	
+	tokenized = []
+	uni = []
+	bi = []
+     	tri = []
+	results = {}
+	for x in input_list:
+		tokenized.append(word_tokenize(x))
+	for x in tokenized:
+		if len(x) == 1:
+			for i in x:
+				uni.append(i)
+		if len(x) == 2:
+			bi.append(x)
+          	if len(x) == 3:
+               		tri.append(x)
+	results['unigrams'] = uni
+     	results['bigrams'] = bi
+     	results['trigrams'] = tri
+	return results
